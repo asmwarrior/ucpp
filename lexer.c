3fb77a04f4dae446b06b6ff60b87f9fef011c6c5
lpsantil <lpsantil@86b33ace-8fdf-11dd-a7bf-a90436dea46c>
2011-07-07 06:28:20 +0000

    Fixed Issue 5 (http://code.google.com/p/ucpp/issues/detail?id=5)
    git-svn-id: https://ucpp.googlecode.com/svn/trunk@4 86b33ace-8fdf-11dd-a7bf-a90436dea46c

--- lexer.c
+++ lexer.c
@@ -626,6 +626,11 @@ static inline int next_char(struct lexer_state *ls)
 		if (c == '\\' && char_lka1(ls) == '\n') {
 			ls->line ++;
 			next_fifo_char(ls);
+		} else if (c == '\r' && char_lka1(ls) == '\n') {
+			ls->line ++;
+			next_fifo_char(ls);
+			c = '\n';
+			return c;
 		} else {
 			ls->last = c;
 			return c;
